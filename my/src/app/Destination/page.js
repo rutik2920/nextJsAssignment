583372c63475c28bf01a309c0f3fc012d6724763
rutik2920 <hrutikjadhav292000@gmail.com>
2024-05-24 21:21:08 +0530

    Update page.js

--- my/src/app/Destination/page.js
+++ my/src/app/Destination/page.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 
 const initial = [
     {id:"London" , image: "https://t3.ftcdn.net/jpg/01/82/88/24/240_F_182882496_2X9iEFq5pAH05uMdF2uMG1oL7Bvvg86Z.jpg"},
-    {id:"New York" , image: "https://t3.ftcdn.net/jpg/06/01/64/42/240_F_601644207_Skh3OczAHkz34jwhbWg4XZDAFBF8AWqs.jpg"},
+    {id:"NewYork" , image: "https://t3.ftcdn.net/jpg/06/01/64/42/240_F_601644207_Skh3OczAHkz34jwhbWg4XZDAFBF8AWqs.jpg"},
     {id:"Tokyo" , image: "https://t4.ftcdn.net/jpg/02/44/31/27/240_F_244312786_qu8X7ywYQokjjvZssbjGsPzkVXQiLBE2.jpg"},
     {id:"Toronto" , image: "https://t3.ftcdn.net/jpg/02/09/58/58/240_F_209585829_NSOqdMwXdO5Q0zsqJyAWzuFSQ69WGlSU.jpg"},
     {id:"Beijing" , image: "https://t3.ftcdn.net/jpg/01/88/89/60/240_F_188896064_5Cc7f11hXLiY73jjcSob0efEwnuSn8Yi.jpg"},
@@ -32,8 +32,8 @@ export default function Home() {
             <h1>Browse by Destination</h1>
             <p>Explore perfect places by Destination</p>
             <div style={{ textAlign: "right", marginTop: "20px",marginBottom:"30px" }}>
-                <button onClick={handlePrev} style={{ marginRight: "10px" }}><img style={{width:"7px"}} src="https://uxwing.com/wp-content/themes/uxwing/download/arrow-direction/arrow-thin-chevron-left-icon.png"></img></button>
-                <button onClick={handleNext}  ><img style={{width:"7px"}} src="https://uxwing.com/wp-content/themes/uxwing/download/arrow-direction/arrow-thin-chevron-right-icon.png"></img></button>
+                <button onClick={handlePrev} style={{ marginRight: "1px",background:"none",border:"none" }}><img style={{width:"15px"}} src="https://cdn-icons-png.flaticon.com/512/860/860790.png"></img></button>
+                <button onClick={handleNext} style={{background:"none",border:"none"}}  ><img style={{width:"15px" }} src="https://www.svgrepo.com/show/27797/right-arrow.svg"></img></button>
             </div>
 
             <div style={{ width: "100%", height:"200px", margin: "auto", display: "grid", gridTemplateColumns:"repeat(4,1fr)" }}>
